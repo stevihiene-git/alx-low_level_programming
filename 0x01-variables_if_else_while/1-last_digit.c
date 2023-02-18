f8c37abb0bfe57b55529d75fd3b6d89a2b2af426
Stephen Oduh <stevihiene@gmail.com>
2023-02-18 14:47:32 -0800

    kl

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
 }
 else if (lastDigit == 0)
 {
-printf("Last digit of %d is %d is 0 and is 0", n, lastDigit);
+printf("Last digit of %d is %d and is 0", n, lastDigit);
 }
 return (0);
 }
