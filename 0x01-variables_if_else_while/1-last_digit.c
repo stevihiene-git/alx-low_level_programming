77529041c8efc68d6f18dbc7fde89f902c01a298
Stephen Oduh <stevihiene@gmail.com>
2023-02-18 14:04:58 -0800

    hg

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit < 6)
 {
-printf("last digit of %d is %d and is less than 6 and not zero", n, lastDigit);
+printf("Last digit of %d is %d and is less than 6 and not zero", n, lastDigit);
 }
 else if (lastDigit > 6)
 {
-printf("last digit of %d is %d and is greater than 5", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
 }
 else
 {
-printf("last digit of %d is %d and is 0 and is 0", n, lastDigit);
+printf("Last digit of %d is %d and is 0 and is 0", n, lastDigit);
 }
 return (0);
 }
