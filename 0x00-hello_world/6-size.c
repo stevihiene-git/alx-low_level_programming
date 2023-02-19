c4571d6b5401c7ff0df77381e05f93886364cd33
Stephen Oduh <stevihiene@gmail.com>
2023-02-19 10:05:32 -0800

    gg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,21 @@
-#include <stdio.h>
-int main(void) { int a; long int b; long long int c; char d; float f; }
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the use of sizeOf() function'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int int_Type;
+float float_Type;
+double double_Type;
+char char_Type;
+/*sizeof evaluates the size of a variable*/
+printf("Size of int: %zu bytes\n", sizeof(int_Type));
+printf("Size of float: %zu bytes\n", sizeof(float_Type));
+printf("Size of double: %zu bytes\n", sizeof(double_Type));
+printf("Size of char: %zu byte\n", sizeof(char_Type));
+return (0);
+}
