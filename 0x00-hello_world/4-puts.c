9a5484a9bd3a011d14aead77e3b2dc34348cdbf8
Stephen Oduh <stevihiene@gmail.com>
2023-02-19 07:14:50 -0800

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the script print using puts'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char myPuts[] = "\"Programming is like building a multilingual puzzle\n";
