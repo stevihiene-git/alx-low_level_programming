ef4278e16cf2ec73a820dc4b3fe6bcd5dece1bb3
Stephen Oduh <stevihiene@gmail.com>
2023-02-20 12:41:01 -0800

    hel

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print alphabets except e and q'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b10 = 0;
+
+	while (b10 <= 10)
+	{
+	printf("%d", b10);
+	b10++;
+	}
+	return (0);
+}
