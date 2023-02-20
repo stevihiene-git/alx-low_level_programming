f767b562dbce9a32292d375d69138a9039d03cbd
Stephen Oduh <stevihiene@gmail.com>
2023-02-20 06:42:55 -0800

    hhhhh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,17 @@
  */
 int main(void)
 {
-char ch2;
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-putchar(ch);
-}
-for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
-{
-putchar(ch2);
-}
-putchar(ch + ch2);
-return (0);
+	char ch2;
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
+	{
+		putchar(ch2);
+	}
+	putchar('\n');
+	return (0);
 }
