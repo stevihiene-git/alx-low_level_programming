5443c8a230a0a41fcd51759d8c3c2a6b2855004e
Stephen Oduh <stevihiene@gmail.com>
2023-02-21 12:25:39 -0800

    0-p

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print _putchar'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+	return (0);
+}
