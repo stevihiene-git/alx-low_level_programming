b9200c8c16a999930468b15112f8de7248993760
Stephen Oduh <stevihiene@gmail.com>
2023-02-24 12:49:41 -0800

    commit2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * positive_or_negative - a function that test for positive or negative integer
- * @main: tests function that prints if integer is positive or negative
+ *@main: tests function that prints if integer is positive or negative
  * Return: 0
  */
 
