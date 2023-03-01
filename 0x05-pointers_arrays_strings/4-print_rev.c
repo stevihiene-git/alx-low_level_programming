36dd77c829b5322e30cceede3004c216b98b5408
Stephen Oduh <stevihiene@gmail.com>
2023-02-28 21:34:09 -0800

    gh

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,22 +6,21 @@
  * @s: arg s
  *
  * Return: void
+ *
  */
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != 0)
 	{
 		i++;
 	}
-
-	i--;
-
-	while (s[i] != '\0')
+	while (i > 0)
 	{
-	_putchar(s[i--]);
+		i--;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
