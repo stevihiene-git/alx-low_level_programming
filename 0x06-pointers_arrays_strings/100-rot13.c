3b2925f748ec3295b4fc1986d8e7875778d5e70c
Stephen Oduh <stevihiene@gmail.com>
2023-03-01 23:30:41 -0800

    ghy

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @words: the string to encode
+ *
+ * Return: encode string
+ */
+char *rot13(char *words)
+{
+	int i, j;
+
+	char alfabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (words[i] != '\0')
+	{
+		j = 0;
+		while (alfabet[j] != '\0')
+		{
+			if (words[i] == alfabet[j])
+			{
+				words[i] = rot[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
