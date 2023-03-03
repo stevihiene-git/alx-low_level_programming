6c3122bb82a0f0db3dcc79313b8d4411f8a0bd08
Stephen Oduh <stevihiene@gmail.com>
2023-03-02 21:14:37 -0800

    look

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,6 +11,10 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
+	int day;
+	int year;
+	int month;	
+
 	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
