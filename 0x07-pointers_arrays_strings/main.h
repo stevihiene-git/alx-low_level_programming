6c71f8a33fe22ac1f52ef1193d78abf4e53c397d
Stephen Oduh <stevihiene@gmail.com>
2023-03-07 09:10:36 -0800

    kill2

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,2 +1,6 @@
 char _putchar(char h);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
