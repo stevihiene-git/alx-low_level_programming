093dc04168319a476be86041acd6482e48e283ef
Stephen Oduh <stevihiene@gmail.com>
2023-03-21 09:17:47 -0700

    klose

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,11 @@
  * @name: name
  * @age: age
  * @owner: owner
+ *Description: Defines metadata about the dog and type of dog's struct
  */
 
 struct dog
-{	
+{
 	char *name;
 	float age;
 	char *owner;
