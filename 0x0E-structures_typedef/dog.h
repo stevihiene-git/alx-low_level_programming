944ed3b1b3bb05fbd142926276bb3c19e3012d44
Stephen Oduh <stevihiene@gmail.com>
2023-03-22 11:16:09 -0700

    fg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - define a new type of struct
  * @name: name's dog
@@ -17,3 +19,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
