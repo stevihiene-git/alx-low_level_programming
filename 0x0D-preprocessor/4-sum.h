4c1ca26016cd9c3ac479fb12ea82d92a761187e8
Stephen Oduh <stevihiene@gmail.com>
2023-03-22 11:38:52 -0700

    op

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -6,6 +6,5 @@
  * Desc: Header file that defines a function-like macro SUM(x, y)
  *       that computes the sum of the numbers x and y.
  */
-
-#define SUM(x, y) ((x)+(y))
+#define SUM(x, y) ((x) + (y))
 #endif
