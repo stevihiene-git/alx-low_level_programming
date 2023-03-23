e8504fc9a229dd785cf6dd2e7745d9a68b1d4f12
Stephen Oduh <stevihiene@gmail.com>
2023-03-23 07:41:10 -0700

    jkl

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "func_poniters.h"
+
+/**
+  * print_name - Prints a name
+  * @name: The name to prints
+  * @f: Pointer to function
+  *
+  * Return: Nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
