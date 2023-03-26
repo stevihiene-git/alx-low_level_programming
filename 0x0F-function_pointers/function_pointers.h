9f1de1f6437b376f9d40c855e7adeec7365a9444
Stephen Oduh <stevihiene@gmail.com>
2023-03-26 08:05:36 -0700

    kk

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+/**
+ * struct size - define a new type of struct
+ * 
+ *  
+ *
+ */
+typedef struct size
+} size_t;
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
