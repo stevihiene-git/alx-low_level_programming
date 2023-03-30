ec94f835124d9ba70124cb320d746d5dfbef8463
Stephen Oduh <stevihiene@gmail.com>
2023-03-30 16:00:32 -0700

    4th commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * listint_len - Returns the number of elements
- *               in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
- *
- * Return: The number of elements in the listint_t list.
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
-size_t listint_len(const listint_t *h)
+
+size_t list_len(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nelem;
 
-	while (h)
+	nelem = 0;
+	while (h != NULL)
 	{
-		nodes++;
 		h = h->next;
+		nelem++;
 	}
-
-	return (nodes);
+	return (nelem);
 }
